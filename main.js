c0210d9099ca75c5a7b19f2ccae257e5c702d9e9
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-10 14:47:21 -0600

    Add the currentTime to be 0 so that it does not play everyonce in a while when you press down on a key quickly

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 window.addEventListener('keydown', (e) => {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-  console.log(audio);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  audio.currentTime = 0; 
+  audio.play();
 })
