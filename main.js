d127b7f7e9ed4c20f50606bb2e331f804209a55b
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-10 21:35:03 -0600

    Add own method of playsound to separate from the addEventListener for ease of readability

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-window.addEventListener('keydown', (e) => {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if(!audio) return;
-  audio.currentTime = 0; 
-  audio.play();
-  key.classList.add('playing');
+window.addEventListener('keydown', playSound );
 
-});
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0; 
+    audio.play();
+    key.classList.add('playing');
+}
 
 function removeTransition(e) {
     if(e.propertyName !== 'transform') return;
